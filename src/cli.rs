308b4069fd034a18eeaebebb12329c2c3e6b5efc
3iqyaan <210480074+3iqyaan@users.noreply.github.com>
2025-12-26 12:03:34 +0530

    Enhance cli, use ignore Walker

--- src/cli.rs
+++ src/cli.rs
@@ -1,12 +1,105 @@
-use clap::{Parser};
+use clap::{Args, Parser, Subcommand};
 
 #[derive(Parser, Debug, Clone)]
 #[command(name = "i-see", version = "1.0", author = "Muhammad", about = "Count the numbr of lines in a file (or some other byte)")]
 pub struct Isee{
+
+    #[command(subcommand)]
+    pub target: Target
+    // /// Give relative or absolute path to the files you want to check in, seperated by spaces
+    // #[arg(short, long)]
+    // pub file: Vec<String>,
+    // /// Give some directory to recursively check all files in it
+    
+    // /// Activate this flag to count the number of lines
+    // #[arg(short, long)]
+    // pub line: bool,
+    // /// Use this flag to count some byte
+    // #[arg(short, long)]
+    // pub byte: Option<u8>,
+    // /// Use this flag to count some literal word (only works if the file is in utf-8)
+    // #[arg(short, long)]
+    // pub word: Option<String>,
+    // /// Activate this flag to make the output more verbose
+    // #[arg(short, long)]
+    // pub verbose: bool,
+    // /// Use this flag to count some unicode character (only works if the file is in utf-8)
+    // #[arg(short, long)]
+    // pub char: Option<char>,
+}
+
+#[derive(Subcommand, Debug, Clone)]
+pub enum Target {
+    File(FCmd),
+    Dir(DCmd)
+}
+
+#[derive(Args, Debug, Clone)]
+pub struct DCmd {
+    /// Activate this flag to analyze files recursively in subdirectories
+    #[arg(short = 'R', long)]
+    pub recursive: bool,
+
+    /// Give relative or absolute path to the directory you want to check in
+    #[arg(short, long)]
+    pub path: String,
+
+    /// Optional: only analyze files with this extension
+    #[arg(short, long)]
+    pub extension: Option<String>,
+
+    /// Use this flag to count some byte
+    #[arg(short, long)]
+    pub byte: Option<u8>,
+
+    /// Activate this flag to count the number of lines
+    #[arg(short, long)]
+    pub line: bool,
+
+    /// Activate this flag to make the output more verbose
+    #[arg(short, long)]
+    pub verbose: bool,
+
+    /// Activate this flag to get a report.json file in the current directory
+    #[arg(short, long)]
+    pub report: bool,
+
+    /// Activate this flag to analyze ignored files based on .gitignore
+    #[arg(short, long)]
+    pub no_gitignore: bool,
+
+    /// Activate this flag to analyze hidden files
+    #[arg(short = 'H', long)]
+    pub hidden: bool,
+}
+
+#[derive(Args, Debug, Clone)]
+pub struct FCmd {
     /// Give relative or absolute path to the files you want to check in, seperated by spaces
     #[arg(short, long)]
     pub file: Vec<String>,
-    /// The byte you want to search in those files
+
+    /// Use this flag to count some byte
     #[arg(short, long)]
     pub byte: Option<u8>,
+
+    /// Activate this flag to count the number of lines
+    #[arg(short, long)]
+    pub line: bool,
+
+    /// Activate this flag to make the output more verbose
+    #[arg(short, long)]
+    pub verbose: bool,
+
+    /// Activate this flag to get a report.json file in the current directory
+    #[arg(short, long)]
+    pub report: bool,
+
+    /// Activate this flag to analyze ignored files based on .gitignore
+    #[arg(short, long)]
+    pub no_gitignore: bool,
+
+    /// Activate this flag to analyze hidden files
+    #[arg(long)]
+    pub no_hidden: bool,
 }
--- src/main.rs
+++ src/main.rs
@@ -13,23 +13,20 @@ fn main() -> std::io::Result<()>{
 
     let args = Isee::parse();
 
-    let file_names = args.file;
-    let byte = match args.byte{
-        Some(b) => b,
-        None => b'\n'
-    };
-    for file_name in file_names{
-        let file_name = file_name.trim();
-        let c = count(&file_name.replace("\"", ""), byte)?;
-        println!("There were {} lines in file: {}", c, file_name);
-        println!("Execution took time: {}ms", start.elapsed().as_millis());
+    match args.target{
+        cli::Target::File(fcmd) => {
+            analyze_file(fcmd)?;
+        }
+        cli::Target::Dir(dcmd) => {
+            analyze_dir(dcmd)?;
+        }
     }
-
+    println!("Execution took time: {}ms", start.elapsed().as_millis());
 
     Ok(())
 }
 
-fn count(file_name: &str, byte: u8) -> std::io::Result<u64>{
+fn countb(file_name: &str, byte: u8) -> std::io::Result<u64>{
 
     let mut buf = [0u8; 65536];
     let mut c: u64 = 0;
@@ -48,4 +45,73 @@ fn count(file_name: &str, byte: u8) -> std::io::Result<u64>{
         }
     }
     Ok(c)
+}
+
+fn analyze_file(args: cli::FCmd) -> std::io::Result<()> {
+    let file_names: Vec<String> = args.file;
+    let v = args.verbose;
+
+    for file_name in file_names{
+        let file_name = file_name.trim();
+        let file_name = file_name.replace("\"", "");
+        if let Some(b) = args.byte{
+            let cb = countb(&file_name, b)?;
+            if v{
+                println!("There were {} occurrences of byte {} in file: {}", cb, b, file_name);
+            }
+        }
+        if args.line{
+            let cl = countb(&file_name, b'\n')?;
+            if v{
+                println!("There were {} lines in file: {}", cl, file_name);
+            }
+        }
+    }
+    Ok(())
+}
+
+fn analyze_dir(args: cli::DCmd) -> std::io::Result<()> {
+    let v = args.verbose;
+    let depth = match args.recursive {
+        true => None,
+        false => Some(1),
+    };
+
+    let walker = ignore::WalkBuilder::new(&args.path)
+                .hidden(!args.hidden)
+                .git_ignore(!args.no_gitignore)
+                .max_depth(depth)
+                .build();
+
+    for path in walker{
+        let path = path.unwrap().path().to_path_buf();
+        if path.is_file(){
+            if let Some(ext) = &args.extension{
+                if let Some(e) = path.extension(){
+                    if e.to_str().unwrap() != ext{
+                        continue;
+                    }
+                }
+                else{
+                    continue;
+                }
+            }
+            if let Some(b) = args.byte{
+                let c = countb(&path.to_string_lossy().to_string(), b)?;
+                if v {
+                    println!("There were {} occurrences of byte {} in file: {}", c, b, path.display());
+                }
+            }
+            if args.line{
+                let c = countb(&path.to_string_lossy().to_string(), b'\n')?;
+                if v {
+                    println!("There were {} lines in file: {}", c, path.display());
+                }
+            }
+        }
+        else if args.recursive && path.is_dir(){
+            
+        }
+    }
+    Ok(())
 }
